bc45d563c15220b1c3a137a1a819acfbbd792f6d
Eduardo Henrique dos Anjos <eduardo.dosanjos@hotmail.com>
2021-10-21 13:27:17 -0300

    Sistema de pontuação adicionado

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -7,12 +7,12 @@ print("*********************************")
 
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
+pontos = 1000
 
-print("\nQual nível de dificuldade você quer?")
+print("\nQual nível de dificuldade?")
 print("(1) Fácil\n(2) Médio\n(3) Difícil")
 
 nivel = int(input("Defina o nível: "))
-
 if nivel == 1:
     total_de_tentativas = 20
 elif nivel == 2:
@@ -23,6 +23,7 @@ print("")
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}')
+
     chute = int(input("Digite um número entre 1 e 100: "))
     print(f'Você digitou: {chute}')
 
@@ -35,12 +36,13 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if acertou:
-        print("Você Acertou!")
+        print(f'Você Acertou e fez {pontos} pontos!')
         break
     else:
         if maior:
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
+        pontos -= (abs(numero_secreto - chute))
 
 print("Fim do Jogo")
